ec5e9cf2e98f770bc5a5a6e634b8911b84bf2669
zhanping <849430904@qq.com>
2018-08-27 09:11:22 +0800

    coder

--- go/goCode/韩顺平代码/chapter06/fundemo01/main/main.go
+++ go/goCode/韩顺平代码/chapter06/fundemo01/main/main.go
@@ -1,7 +1,17 @@
 package main
 import (
 	"fmt"
-	"go_code/chapter06/fundemo01/utils"
+	"go_code/chapter06/fundemo01/utils"//引入包
+
+	/**
+
+	在 import 包时，路径从 $GOPATH 的 src 下开始，不用带 src , 编译器会自动从 src 下开始引入
+	为了让其它包的文件，可以访问到本包的函数，则该函数名的首字母需要大写，类似其它语言 的 public ,这样才能跨包访问。比如 utils.go 的
+	在访问其它包函数，变量时，其语法是 包名.函数名， 比如这里的 main.go 文件中
+	如果包名较长，Go 支持给包取别名， 注意细节:取别名后，原来的包名就不能使用了
+	如果你要编译成一个可执行程序文件，就需要将这个包声明为 main , 即 package main .这个就
+    是一个语法规范，如果你是写一个库 ，包名可以自定义
+	**/
 )
 
 
