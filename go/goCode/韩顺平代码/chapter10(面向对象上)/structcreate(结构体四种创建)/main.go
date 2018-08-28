83d544fc06e327402767c7474ef100877e785900
849430904 <849430904@qq.com>
2018-08-28 23:30:43 +0800

    map与结构体一

--- go/goCode/韩顺平代码/chapter10(面向对象上)/structcreate(结构体四种创建)/main.go
+++ go/goCode/韩顺平代码/chapter10(面向对象上)/structcreate(结构体四种创建)/main.go
@@ -3,6 +3,26 @@ import (
 	"fmt"
 )
 
+/**
+
+创建结构体变量和访问结构体字段
+
+方式 1-直接声明 
+   案例演示: var person Person
+方式 2-{}
+   案例演示: var person Person = Person{}
+方式 3-&
+   案例: var person *Person = new (Person)
+方式 4-{}
+   案例: var person *Person = &Person{}
+
+说明:
+	1) 第 3 种和第 4 种方式返回的是 结构体指针。
+	2) 结构体指针访问字段的标准方式应该是:(*结构体指针).字段名 ，比如 (*person).Name = "tom" 
+	3) 但 go 做了一个简化，也支持 结构体指针.字段名, 比如 person.Name = "tom"。更加符合程序员
+	使用的习惯，go 编译器底层 对 person.Name 做了转化 (*person).Name。
+**/
+
 type Person struct{
 	Name string
 	Age int
